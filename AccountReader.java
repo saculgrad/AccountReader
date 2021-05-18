ab5a3e124b96e2f86947a624c92e31b0d2f2e790
saculgrad <70218089+saculgrad@users.noreply.github.com>
2021-05-18 09:29:44 -0700

    Did entire project
    Did the project i guess

---
+++ AccountReader.java
@@ -0,0 +1,51 @@
+import java.util.*;
+import java.io.*;
+
+public class AccountReader extends Thread{
+	public static void main (String [] args){
+    AccountReader account = new AccountReader();
+		account.checkAccountBalances("Accounts/Accounts 0.txt");
+    account.checkAccountBalances("Accounts/Accounts 2.txt");
+	}
+
+	public String checkAccountBalances (String filename){
+		double maxNum = 0;
+    String maxAccount = "";
+		try{
+			File myObj = new File(filename);
+      		Scanner myReader = new Scanner(myObj);
+      		while (myReader.hasNextLine()) {
+        		String data = myReader.nextLine();
+        		int cutoff = data.indexOf ("\t");
+        		String numWord = data.substring (cutoff,data.length());
+        		double num = Double.parseDouble (numWord);
+            if (num > maxNum){
+              maxNum = num;
+              maxAccount = data;
+            }
+        		
+
+      		}
+      		myReader.close();
+          
+    	}catch (FileNotFoundException e) {
+      		System.out.println("An error occurred. Ha nerd.");
+      		e.printStackTrace();
+      }
+      //System.out.println ("done with "+filename);
+      //System.out.println (maxAccount);
+      return maxAccount;
+
+    }
+
+    public void run (){
+      try {
+          System.out.println("Thread " + Thread.currentThread().getId() + " is running");
+      }catch (Exception e) {
+          System.out.println("Exception is caught");
+          e.printStackTrace();
+      }
+    }
+
+}
+
---
+++ Multithread.java
@@ -0,0 +1,29 @@
+public class Multithread{
+  public static void main(String[] args){
+		long start = System.currentTimeMillis();        
+		int n = 10;
+		String maxAccount = "";
+		double maxValue = 0.0;
+        for (int i = 0; i < n; i++) {
+            AccountReader object= new AccountReader();
+            object.start();
+            String filename = "Accounts/Accounts "+i+".txt";
+            String data = object.checkAccountBalances (filename);
+            int cutoff = data.indexOf ("\t");
+            String numWord = data.substring (cutoff,data.length());
+        	double num = Double.parseDouble (numWord);
+        	if (num > maxValue){
+              maxValue = num;
+              maxAccount = data.substring (0,cutoff-2);
+            }
+        }
+        System.out.println(maxAccount);
+        
+
+        long finish = System.currentTimeMillis();
+        long timeElapsed = finish - start;
+        double time = (double )timeElapsed/1000;
+        System.out.println (time);
+
+    }
+}
